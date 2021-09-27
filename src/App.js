1c339b0ef3541d280f6395579b0b8f85a09e8d6d
Rabin Gaire <rabingaire20@gmail.com>
2021-09-27 13:35:39 +0545

    fix css on table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import Table from "./Table.js"
+import Table from "./Table.js";
 
 function App() {
   return (
     <div className="App">
-      <Table projectKey="b03c85ap_AZUVBCjNXioYPWkLvP4uska5baWiH2uE" driveName="drawings"/>
-
+      <Table
+        projectKey={process.env.REACT_APP_PROJECT_KEY}
+        driveName={process.env.REACT_APP_DRIVE_NAME}
+      />
     </div>
   );
 }
